c011cd7585bfe9dcb02b214b7db778fdc98b4d64
SnoopyXX <elfprest@sina.com>
2018-07-10 16:00:36 +0800

    QuickHit v0.1

--- src/com/QuickHit/Player.java
+++ src/com/QuickHit/Player.java
@@ -1,5 +1,7 @@
 package com.QuickHit;
 
+import java.util.Scanner;
+
 public class Player {
     private int levelNo;
     private int curScore;
@@ -38,7 +40,23 @@ public class Player {
         this.eclpsedTime = eclpsedTime;
     }
 
+    /*
+        玩家玩游戏
+     */
     public void player(){
+        Game game = new Game();
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < 6; i++) {
+            this.levelNo += 1;//晋级
+            this.startTime = System.currentTimeMillis();
+            this.curScore = 0;//积分清0
 
+            //循环比较字符串的输入、输出
+            for (int j = 0; j < new Level().getStrTimes(); j++) {
+                String outStr = game.printStr();//系统输出
+                String inputStr = input.next();//用户输入
+                game.printResult(inputStr,outStr);//结果
+            }
+        }
     }
 }
